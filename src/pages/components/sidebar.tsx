9ee01fab7d4e0d9e902d143203e1942b58d74038
suniason <galvezbrett17@gmail.com>
2023-08-28 23:40:29 +0800

    added some css changes

--- src/pages/components/sidebar.tsx
+++ src/pages/components/sidebar.tsx
@@ -9,25 +9,25 @@ const Sidebar: React.FC<SidebarProps> = ({ isHidden, setView }) => {
   return (
     <div
       className={`${
-        isHidden ? '-translate-x-full overflow-hidden w-0 ' : 'translate-x-0 px-3 py-2'
-      } text-white h-screen transition-transform bg-slate-800`}
+        isHidden ? '-translate-x-full overflow-hidden w-0 ' : 'translate-x-0 px-3'
+      } text-white h-screen transition-transform bg-slate-800 py-5`}
     >
       <div className='text-2xl text-center px-4 font-bold'>Learn Japanese</div>
-      <div className={`grid py-2 text-md font-semibold`}>
+      <div className='grid py-2 text-md font-semibold my-2'>
         <p
-          className='px-8 py-2 hover:bg-white hover:bg-opacity-10 rounded-md transition-all cursor-pointer'
+          className='px-4 py-2 hover:bg-white hover:bg-opacity-10 rounded-md transition-all cursor-pointer'
           onClick={() => setView('overview')}
         >
           Overview
         </p>
         <p
-          className='px-8 py-2 hover:bg-white hover:bg-opacity-10 rounded-md transition-all cursor-pointer'
+          className='px-4 py-2 hover:bg-white hover:bg-opacity-10 rounded-md transition-all cursor-pointer'
           onClick={() => setView('character')}
         >
-          Japanese Characters
+          Characters
         </p>
         <p
-          className='px-8 py-2 hover:bg-white hover:bg-opacity-10 rounded-md transition-all cursor-pointer'
+          className='px-4 py-2 hover:bg-white hover:bg-opacity-10 rounded-md transition-all cursor-pointer'
           onClick={() => setView('phrase')}
         >
           Common Phrases
